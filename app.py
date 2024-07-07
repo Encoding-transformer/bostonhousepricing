eebb1ea9657b97d26b5463c3c514a0d559d29c34
Encoding-transformer <shubhamyadav11jan@gmail.com>
2024-07-07 16:52:25 +0530

    Ready web app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pickle
 from flask import Flask, request, app, jsonify, url_for , render_template
-
 import numpy as np 
 import pandas as pd
 
@@ -21,7 +20,15 @@ def predict_api():
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
-    return jsonify(ouput=0)
+    return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text=f"The predicted house price is {output}")
 
 if __name__=="__main__":
     app.run(debug=True)
